2d0f9eed41ad0b2a78f1bfff496f32fe9e9a8002
Jacob Coats <tjcoats97@gmail.com>
2020-04-21 14:57:29 -0500

    created buoy weather component

---
+++ Application/MAPP/src/app/weather/buoy-weather/buoy-weather.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ns-buoy-weather',
+  templateUrl: './buoy-weather.component.html',
+  styleUrls: ['./buoy-weather.component.css']
+})
+export class BuoyWeatherComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
