2dfa9f5771dab450b56a46160f9706f8db311084
Austin Herriott <ajherriott@crimson.ua.edu>
2020-04-27 13:24:55 -0400

    Baseline Code for Programatic implementation of MapBox
    Code doesn't work in its current state but should provide a solid base to work from for declaring the map programatically to replace the existing implementation

--- Application/MAPP/src/app/home/home.component.ts
+++ Application/MAPP/src/app/home/home.component.ts
@@ -1,14 +1,18 @@
 import { BuoyService } from "../shared/buoy.service";
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild, ViewContainerRef } from "@angular/core";
 import { DataService, DataItem } from "../shared/data.service";
 import { map } from "rxjs/operators";
-import { MapboxMarker, MapboxViewApi } from "nativescript-mapbox";
-import { Observable } from 'rxjs';
+import { Observable } from "tns-core-modules/data/observable";
 import { registerElement } from "nativescript-angular/element-registry";
 import { ModalDialogHost } from "nativescript-angular";
 import * as dialogs from "tns-core-modules/ui/dialogs";
+import { Mapbox, MapStyle, OfflineRegion, LatLng, Viewport, DownloadProgress, MapboxMarker, MapboxViewApi, MapboxView } from "nativescript-mapbox";
+import * as platform from "tns-core-modules/platform";
 
 registerElement("Mapbox", () => require("nativescript-mapbox").MapboxView);
+const isIOS = platform.device.os === platform.platformNames.ios;
+const ACCESS_TOKEN = "sk.eyJ1IjoiYXdpbWFwcCIsImEiOiJjazkwMWxtamEwMDBhM2hwdXF5dmlkaTlpIn0.4we_OHFl1VMXDbpJ0tq48A";
+const MAP_STYLE = "mapbox://styles/awimapp/ck901qzms0xwq1iqgdg61jijj"
 
 @Component({
     selector: "Home",
@@ -17,216 +21,104 @@ registerElement("Mapbox", () => require("nativescript-mapbox").MapboxView);
 })
 
 export class HomeComponent implements OnInit {
-    items: Array<DataItem>;
-    buoyData;
-    locationData;
-    mapbox: MapboxViewApi
+  private mapbox: Mapbox;
+  private mapView = new MapboxView();
 
-    constructor(private _itemService: DataService, private buoyService: BuoyService) { }
-
+  constructor() {
+    //super();
+    this.mapbox = new Mapbox();
+  }
     ngOnInit(): void {
-        
-        this.buoyService.getBuoyData().subscribe(d => {
-            this.buoyData = d;
-        });
-        
-        /*
-        this.items = this._itemService.getItems();
-        */
+      throw new Error("Method not implemented.");
     }
 
-    onMapReady(args) {
-        
-        this.mapbox = args.map;
-        /*
-        this.mapbox.getUserLocation().then(
-            function (userLocation) {
-                alert("Current user location: " + userLocation.location.lat + ", " + userLocation.location.lng);
-                console.log("Current user speed: " + userLocation.speed);
-            }
-        )
-        
-        this.mapbox.addMarkers([
-            <MapboxMarker>{
-                id: 1,
-                lat: 25,
-                lng: -90.5,
-                title: '15',
-                iconPath: 'app/home/assets/marker15.png',
-                onTap: marker => console.log("Buoy ID: '" + marker.title + "'"),
-                onCalloutTap: marker => alert("please work")
-            },
-            <MapboxMarker>{
-                id: 1,
-                lat: 24.7,
-                lng: -90.1,
-                title: '20',
-                iconPath: 'app/home/assets/marker20.png',
-                onTap: marker => console.log("Buoy ID: '" + marker.title + "'"),
-                onCalloutTap: marker => alert("please work")
-            },
-            <MapboxMarker>{
-                id: 1,
-                lat: 24.3,
-                lng: -89.8,
-                title: '8',
-                iconPath: 'app/home/assets/marker8.png',
-                onTap: marker => console.log("Buoy ID: '" + marker.title + "'"),
-                onCalloutTap: marker => alert("please work")
-            },
-            <MapboxMarker>{
-                id: 1,
-                lat: 24,
-                lng: -89.5,
-                title: 'Test Marker',
-                iconPath: 'app/home/assets/blackMarker14px.png',
-                onTap: marker => console.log("Buoy ID: '" + marker.title + "'"),
-                onCalloutTap: marker => alert("please work")
-            },
-            <MapboxMarker>{
-                id: 1,
-                lat: 23.5,
-                lng: -89,
-                title: '12',
-                iconPath: 'app/home/assets/marker12.png',
-                onTap: marker => console.log("Buoy ID: '" + marker.title + "'"),
-                onCalloutTap: marker => alert("please work")
-            },
-        ])
-        */
-    }
+    @ViewChild( 'mapContainer', { read: ViewContainerRef, static: false }) mapContainer;
 
-    toggleBuoyMarkers() {
+    public mapboxView: MapboxViewApi = null;
+    public mapboxApi: Mapbox = null;
 
-            this.buoyService.getBuoyData().subscribe(el => {
-                //pull array of buoys
-                this.buoyData = el;
-                var buoyCount = (this.buoyData.length)-1;
+  public doShow(): void {
+    this.mapbox.show({
+      accessToken: ACCESS_TOKEN,
+      //Style will need to be adjusted based on existing AWI Mapbox Account
+      style: MapStyle.TRAFFIC_DAY,
+      //Margins will need to be adjusted
+      margins: {
+        left: 18,
+        right: 18,
+        top: isIOS ? 390 : 454,
+        bottom: isIOS ? 50 : 8
+      },
+      //(24.5, -90) for Gulf of Mexico
+      center: {
+        lat: 24.5,
+        lng: -90
+      },
+      //Made need to adjust some settings 
+      zoomLevel: 3.5, // 0 (most of the world) to 20, default 0
+      showUserLocation: true, // default false
+      hideAttribution: true, // default false
+      hideLogo: true, // default false
+      hideCompass: false, // default false
+      disableRotation: false, // default false
+      disableScroll: false, // default false
+      disableZoom: false, // default false
+      disableTilt: false, // default false
+      //Placeholder marker to be replaced by existing ones
+      markers: [
+        {
+          id: 1,
+          lat: 52.3732160,
+          lng: 4.8941680,
+          title: 'Nice location',
+          subtitle: 'Really really nice location',
+          iconPath: 'res/markers/green_pin_marker.png',
+          onTap: () => console.log("'Nice location' marker tapped"),
+          onCalloutTap: () => console.log("'Nice location' marker callout tapped")
+        }
+      ]
+    }).then(
+        //Supposed to display programatically declared map view, see 'Future Project Enhancements'
+        showResult => {
+          console.log(`Mapbox show done for ${showResult.ios ? "iOS" : "Android"}, native object received: ${showResult.ios ? showResult.ios : showResult.android}`);
 
-                //loop through array of buoys
-                var i;
-                for (i = 0; i < buoyCount; i++) {
-                    // check if buoy is in the gulf of mexico 
-                    if (this.buoyData[i].latitude <= 32 && this.buoyData[i].latitude >= 18 
-                            && this.buoyData[i].longitude <= -78 && this.buoyData[i].longitude >= -99) {
+          this.mapbox.setOnMapClickListener(point => console.log(`>> Map clicked: ${JSON.stringify(point)}`));
 
-                        let windDirection: string = '';
-                        let waveDirection: string = '';
-                        let gustDirection: string = '';
-                        let buoyDescription: string = '';
+          this.mapbox.setOnScrollListener((point: LatLng) => {
+            // console.log(`>> Map scrolled: ${JSON.stringify(point)}`);
+          });
 
-                        if (this.buoyData[i].windDirection != 'MM') {
-                            if (this.buoyData[i].windDirection > 11.25 && this.buoyData[i].windDirection <= 33.75) {
-                                windDirection = "NNE" + '\n';
-                            } else if (this.buoyData[i].windDirection > 33.75 && this.buoyData[i].windDirection < 56.25) {
-                                windDirection = "ENE" + '\n';
-                            } else if (this.buoyData[i].windDirection > 56.25 && this.buoyData[i].windDirection < 78.75) {
-                                windDirection = "E" + '\n';
-                            } else if (this.buoyData[i].windDirection > 78.75 && this.buoyData[i].windDirection < 101.25) {
-                                windDirection = "ESE" + '\n';
-                            } else if (this.buoyData[i].windDirection > 101.25 && this.buoyData[i].windDirection < 123.75) {
-                                windDirection = "ESE" + '\n';
-                            } else if (this.buoyData[i].windDirection > 123.75 && this.buoyData[i].windDirection < 146.25) {
-                                windDirection = "SE" + '\n';
-                            } else if (this.buoyData[i].windDirection > 146.25 && this.buoyData[i].windDirection < 168.75) {
-                                windDirection = "SSE" + '\n';
-                            } else if (this.buoyData[i].windDirection > 168.75 && this.buoyData[i].windDirection < 191.25) {
-                                windDirection = "S" + '\n';
-                            } else if (this.buoyData[i].windDirection > 191.25 && this.buoyData[i].windDirection < 213.75) {
-                                windDirection = "SSW" + '\n';
-                            } else if (this.buoyData[i].windDirection > 213.75 && this.buoyData[i].windDirection < 236.25) {
-                                windDirection = "SW" + '\n';
-                            } else if (this.buoyData[i].windDirection > 236.25 && this.buoyData[i].windDirection < 258.75) {
-                                windDirection = "WSW" + '\n';
-                            } else if (this.buoyData[i].windDirection > 258.75 && this.buoyData[i].windDirection < 281.25) {
-                                windDirection = "W" + '\n';
-                            } else if (this.buoyData[i].windDirection > 281.25 && this.buoyData[i].windDirection < 303.75) {
-                                windDirection = "WNW" + '\n';
-                            } else if (this.buoyData[i].windDirection > 303.75 && this.buoyData[i].windDirection < 326.25) {
-                                windDirection = "NW" + '\n';
-                            } else if (this.buoyData[i].windDirection > 326.25 && this.buoyData[i].windDirection < 348.75) {
-                                windDirection = "NNW" + '\n';
-                            } else {
-                                windDirection = "N" + '\n';
-                            }
-                        } else {
-                            windDirection = '\n';
-                        }
-                        if (this.buoyData[i].windSpeed != 'MM' && this.buoyData[i].windSpeed != '0') {
-                            buoyDescription = buoyDescription + "Wind: " + this.buoyData[i].windSpeed + " m/s " + windDirection;
-                        }
-                        if (this.buoyData[i].gustDirection) {
-                            gustDirection = this.buoyData[i].gustDirection + '\n';
-                        } else {
-                            gustDirection = '\n';
-                        }
-                        if (this.buoyData[i].gustSpeed != 'MM') {
-                            buoyDescription = buoyDescription + "Gusts: " + this.buoyData[i].gustSpeed + " m/s " + gustDirection;
-                        }
-                        if (this.buoyData[i].waveDirection != 'MM') {
-                            waveDirection = this.buoyData[i].waveDirection + '\n';
-                        } else {
-                            waveDirection = '\n';
-                        }
-                        if (this.buoyData[i].waveHeight != 'MM') {
-                            buoyDescription = buoyDescription + "Waves: " + this.buoyData[i].waveHeight + " meters " + waveDirection;
-                        }
-                        if (this.buoyData[i].airTemperature != 'MM') {
-                            buoyDescription = buoyDescription + "Air Temperature: " + this.buoyData[i].airTemperature +"° C" + '\n';
-                        }
-                        if (this.buoyData[i].waterTemperature != 'MM') {
-                            buoyDescription = buoyDescription + "Water Temperature: " + this.buoyData[i].waterTemperature + "° C"  +'\n';
-                        }
+          this.mapbox.setOnFlingListener(() => {
+            console.log(`>> Map flinged"}`);
+          }).catch(err => console.log(err));
+        },
+        (error: string) => console.log("mapbox show error: " + error)
+    );
+  }
 
-                        // create temporary variable to hold marker
-                        var markerZ = <MapboxMarker>{
-                            id: i,
-                            lat: this.buoyData[i].latitude,
-                            lng: this.buoyData[i].longitude,
-                            title: this.buoyData[i].buoyNumber,
-                            iconPath: 'app/home/assets/marker12.png',
-                            onTap: marker => console.log("Buoy ID: '" + marker.title + "'"),
-                            onCalloutTap: marker =>
-                                dialogs.alert({
-                                    title: "Buoy ID: " + marker.title,
-                                    message: "Longitude: '" + marker.lng + "'" + '\n'
-                                        + "Latitude: '" + marker.lat + "'" + '\n'
-                                        + buoyDescription,
-                                    okButtonText: "Okay"
-                                }).then(() => {
-                                    console.log("Dialog closed!");
-                                })
-                        }
 
-                        // add marker to mapview
-                        this.mapbox.addMarkers([
-                            markerZ
-                        ])
-                    }
-                }
-            });
-    }
-
-    toggleWaves() {
-        this.mapbox.removeMarkers();
-    }
-
-    toggleWind() {
-        this.mapbox.removeMarkers();
-    }
-
-    toggleReefs() {
-        this.mapbox.removeMarkers();
-    }
-
-    toggleWaterTemp() {
-        this.mapbox.removeMarkers();
-    }
-
-    toggleChlorophyll() {
-        this.mapbox.removeMarkers();
-    }
-    clearMap() {
-        this.mapbox.removeMarkers();
-    }
+//These functions currently remove markers but should also toggle between overlays when finished
+toggleWaves() {
+    this.mapbox.removeMarkers();
 }
+
+toggleWind() {
+    this.mapbox.removeMarkers();
+}
+
+toggleReefs() {
+    this.mapbox.removeMarkers();
+}
+
+toggleWaterTemp() {
+    this.mapbox.removeMarkers();
+}
+
+toggleChlorophyll() {
+    this.mapbox.removeMarkers();
+}
+clearMap() {
+    this.mapbox.removeMarkers();
+}
+}
+
